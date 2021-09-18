c9908430d5b95aa293c4be3908592740698c9ee7
tranduc04 <85269832+tranduc04@users.noreply.github.com>
2021-09-18 23:08:03 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    introTitle: 'Diệp xinh gái à ^^!',
+    introTitle: 'Chi xinh gái à ^^!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
@@ -13,7 +13,7 @@ const CONFIG = {
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <3333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 8h anh qua đón nhé công chúa.',
+    messDesc: 'Chiều mai anh qua đón nhé công chúa.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://www.facebook.com/tranducdzl' 
 }
